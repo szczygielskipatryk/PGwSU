abb39ff99688dc161f860743d59faad62448bf83
Patryk Szczygielski <patryk8199@gmail.com>
2020-10-26 11:39:30 +0100

    Zmiana Update() na FixedUpdate()

--- zajecia3/Assets/Scripts/Sterowanie.cs
+++ zajecia3/Assets/Scripts/Sterowanie.cs
@@ -11,7 +11,7 @@ public class Sterowanie : MonoBehaviour
         Rb = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
         float mH = Input.GetAxis("Horizontal");
         float mV = Input.GetAxis("Vertical");
