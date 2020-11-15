385b48f1bdff2ea2ef2f493035c700006e9c6b6b
Patryk Szczygielski <patryk8199@gmail.com>
2020-11-15 19:53:49 +0100

    zad 5

---
+++ zajecia5/New Unity Project/Assets/Scripts/Drzwi.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drzwi : MonoBehaviour
+{
+    private Animator animator;
+    private bool drzwi_open; 
+    void Start()
+    {
+        drzwi_open = false;
+        animator = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            drzwi_open = true;
+            Kontroler("open");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            drzwi_open = false;
+            Kontroler("close");
+        }
+    }
+
+    void Kontroler(string kierunek)
+    {
+        animator.SetTrigger(kierunek);
+    }
+}
---
+++ zajecia5/New Unity Project/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public Vector3 cel;
+    private float speed = 3;
+    private Rigidbody rb;
+    private Vector3[] points = new Vector3[2];
+    private int aktual = 0;
+    private bool czyjest = false;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        points[0] = transform.position;
+        points[1] = cel;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.transform.parent = transform;
+            czyjest = true;
+        }
+            
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.transform.parent = null;
+            czyjest = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (czyjest)
+        { 
+            if (transform.position != points[aktual])
+            {
+                Vector3 pos = Vector3.MoveTowards(transform.position, points[aktual], Time.deltaTime * speed);
+                rb.MovePosition(pos);
+            }
+            else
+            {
+                aktual = (aktual + 1) % points.Length;
+            }
+        }
+    }
+
+
+
+}
